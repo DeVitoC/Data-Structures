ee09a42daf17e26aa36c6ca39036cc8ffb6e06cd
Christopher DeVito <christopher.devito@protonmail.com>
2020-06-08 12:52:10 -0400

    Added ChristopherDeVito branch.

--- stack/stack.py
+++ stack/stack.py
@@ -12,7 +12,7 @@ return elements in Last In First Out order.
 """
 class Stack:
     def __init__(self):
-        self.size = 0
+        self.size = 0 
         self.storage = []
 
     def __len__(self):
